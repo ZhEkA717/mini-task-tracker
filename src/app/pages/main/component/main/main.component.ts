0e337d5466aae19a63f9eacbcd4914c745787bc9
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-03-23 01:24:42 +0300

    feat: add greetings

--- src/app/pages/main/component/main/main.component.ts
+++ src/app/pages/main/component/main/main.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DatePeriod } from 'src/app/shared/utils/constants';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export default class MainComponent {
+
+export default class MainComponent implements OnInit, OnDestroy {
+  date = new Date();
+
+  timerId!: ReturnType<typeof setTimeout>;;
+
   links = ['To come', 'Overdue', 'Done'];
 
   activeLink = this.links[0];
+
+  period!: (typeof DatePeriod)[keyof typeof DatePeriod];
+
+
+  ngOnInit(): void {
+    this.timerId = setInterval(() => {
+      this.date = new Date();
+
+      let hour = this.date.getHours();
+
+      if (hour > 0 &&  hour < 6) {
+        this.period = DatePeriod.NIGHT;
+      } else if (hour > 6 && hour < 12) {
+        this.period = DatePeriod.MORNING;
+      } else if (hour > 12 && hour < 18) {
+        this.period = DatePeriod.AFTERNOON;
+      } else {
+        this.period = DatePeriod.EVENING;
+      }
+
+    })
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerId);
+  }
 
   changeFilter(link: string) {
     this.activeLink = link;
---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,6 @@
+export enum DatePeriod {
+  MORNING = "morning",
+  AFTERNOON = 'afternoon',
+  EVENING ='evening',
+  NIGHT='night'
+}
