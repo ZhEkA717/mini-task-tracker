7bd45f17cef295262ea5c880fdaa2c3936a14407
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-03-23 14:56:16 +0300

    feat: add http-api service

--- src/app/shared/models/task.model.ts
+++ src/app/shared/models/task.model.ts
@@ -1,12 +1,17 @@
 import { TypeUser } from './user.model';
 
-export type TaskType = {
+export type Task = {
+  id: string
   title: string;
   name: string,
-  deadline: string,
+  deadline: number,
   priority: TypePriority,
   status: boolean,
   performers: TypeUser[]
 };
+
+export type CreateTaskDto = Omit<Task, 'id'>;
+
+export type UpdateTaskDto = Partial<CreateTaskDto>;
 
 type TypePriority = 'low' | 'medium' | 'high';
